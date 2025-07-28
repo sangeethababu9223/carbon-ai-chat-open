71169b1445ea7307c8fcc3c717bec42c384d16b3
Sangeetha Babu <sangeetha9223@gmail.com>
2025-07-28 20:20:16 +0530

    chore(Carbon React to Carbon web components): InlineNotification (#238)
    * chore(InlineNotification): React to Web Components
    
    * fix(inlinenotification): format issue

---
+++ packages/ai-chat/src/chat/react/carbon/InlineNotification.tsx
@@ -0,0 +1,28 @@
+/*
+ *  Copyright IBM Corp. 2025
+ *
+ *  This source code is licensed under the Apache-2.0 license found in the
+ *  LICENSE file in the root directory of this source tree.
+ *
+ *  @license
+ */
+
+import { createComponent } from "@lit/react";
+import React from "react";
+
+// Export the actual class for the component that will *directly* be wrapped with React.
+import CarbonInlineNotificationElement from "@carbon/web-components/es-custom/components/notification/inline-notification.js";
+
+const InlineNotification = createComponent({
+  tagName: "cds-custom-inline-notification",
+  elementClass: CarbonInlineNotificationElement,
+  react: React,
+}) as React.ForwardRefExoticComponent<
+  React.HTMLAttributes<HTMLElement> & {
+    kind?: string;
+    lowContrast?: boolean;
+    hideCloseButton?: boolean;
+  }
+>;
+
+export default InlineNotification;
